4447fee52f3a442d717d65d2700acc303e326134
Awnish Bhatt <awnishbhatt@yahoo.com>
2014-08-04 21:33:25 -0400

    Commit from eclipse

---
+++ init-git/src/main/java/net/awn/init/git/Sample.java
@@ -0,0 +1,10 @@
+package net.awn.init.git;
+
+public class Sample {
+
+  public Sample() {
+    System.out
+        .println("This beautiful sysout does nothing.. in a real project.. a big no no");
+  }
+
+}
---
+++ init-git/src/test/java/net/awn/init/git/SampleTest.java
@@ -0,0 +1,14 @@
+package net.awn.init.git;
+
+import static junit.framework.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+  @Test
+  public void testName() throws Exception {
+    assertEquals(true, true);
+  }
+
+}
